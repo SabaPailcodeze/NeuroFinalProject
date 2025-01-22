c1a9275b92478991f72f88113959b1775b1ff162
saba pailodze <159852979+pailodzesaba@users.noreply.github.com>
2025-01-22 22:22:14 +0400

    Changed folder names, added new images and code

---
+++ app/Components/Organisms/HeroSection/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className=" h-[100vh] w-[100%] flex">
+      {/* this is nav bar which will be separated as component in future */}
+      <div className="bg-[#666666] px-6 h-full">nabvar</div>
+      <div className="bg-black opacity-[0.5] w-full h-full">main</div>
+    </div>
+  );
+};
+
+export default Hero;
