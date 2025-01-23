6b8a4fa43234e7e5836f6dd0429fffc8eef82f6a
saba pailodze <159852979+pailodzesaba@users.noreply.github.com>
2025-01-23 22:46:54 +0400

    feat: implement hero section layout and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,20 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        supreme: ["Supreme", "sans-serif"],
+      },
       colors: {
         "c-gray": "rgba(0, 0, 0, 0.5)",
         "c-yellow": "#E5E548",
+      },
+      backgroundImage: {
+        image1: "url('/Images/image1.jpg')",
+        image3: "url('/Images/image3.jpg')",
+        image5: "url('/Images/image5.jpg')",
+      },
+      video: {
+        video: "url('/video/video.mp4')",
       },
     },
   },
