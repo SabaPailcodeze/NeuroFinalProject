939ff51e392fc7d3717321feb3bd7b34b7f46c0f
guramius <96048349+guramius@users.noreply.github.com>
2025-01-27 20:18:27 +0400

    done

--- app/Components/Organisms/Main/Main.tsx
+++ app/Components/Organisms/Main/Main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import WeBuilt from "../../Molecules/WeBuilt/WeBuilt";
 import LatestProject from "../../Molecules/LatestProject/LatestProject";
+import StudioServicesMarge from "../../Molecules/StudioServicesMarge/StudioServicesMarge";
 
 const Main = () => {
   return (
     <div className="px-[150px] bg-black">
       <WeBuilt />
       <LatestProject/>
+      <StudioServicesMarge/>
     </div>
   );
 };
