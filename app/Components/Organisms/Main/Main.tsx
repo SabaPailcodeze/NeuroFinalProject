b0b923562369a4c2aa15e94a73f51780c42132bf
saba pailodze <159852979+pailodzesaba@users.noreply.github.com>
2025-01-29 02:46:32 +0400

    Added slider component

--- app/Components/Organisms/Main/Main.tsx
+++ app/Components/Organisms/Main/Main.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import WeBuilt from "../../Molecules/WeBuilt/WeBuilt";
 import LatestProject from "../../Molecules/LatestProject/LatestProject";
 import StudioServicesMarge from "../../Molecules/StudioServicesMarge/StudioServicesMarge";
+import Slider from "../../Molecules/Slider/Slider";
 
 const Main = () => {
   return (
     <div className="px-[150px] bg-black">
       <WeBuilt />
-      <LatestProject/>
-      <StudioServicesMarge/>
+      <LatestProject />
+      <StudioServicesMarge />
+      <Slider />
     </div>
   );
 };
