d760d130666f6b0bbc8f2017b3fb9fdf85f5d7d1
guramius <96048349+guramius@users.noreply.github.com>
2025-01-31 23:33:12 +0400

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-// import Footer from "./Components/Organisms/Footer/Footer";
-// import Hero from "./Components/Organisms/HeroSection/HeroSection";
+import Footer from "./Components/Organisms/Footer/Footer";
+import Hero from "./Components/Organisms/HeroSection/HeroSection";
 import Main from "./Components/Organisms/Main/Main";
 
 const Page = () => {
   return (
     <div className="w-full h-full">
-      {/* <Hero /> */}
+      <Hero />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
