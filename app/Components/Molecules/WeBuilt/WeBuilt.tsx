8a7011f74057d1b11eb429edfce48b0a2ede5a6b
guramius <96048349+guramius@users.noreply.github.com>
2025-02-01 00:32:12 +0400

    done

--- app/Components/Molecules/WeBuilt/WeBuilt.tsx
+++ app/Components/Molecules/WeBuilt/WeBuilt.tsx
@@ -16,7 +16,7 @@ const WeBuilt = ({
       <div className="text-white"></div>
       <div
         id="WeBuilt"
-        className="py-[160px] font-supreme flex flex-col gap-12 h-[100vh]"
+        className="font-supreme flex flex-col gap-12 h-[100vh]"
       >
         <div className=" text-white">
           <h1
